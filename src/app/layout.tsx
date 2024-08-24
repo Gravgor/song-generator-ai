2f8d3367eefef61abfeb0e3b76e5152d5975a9f2
Marceli Borowczak <marcelborowczak@outlook.com>
2024-08-24 18:09:05 +0200

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,6 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <SpeedInsights />
         <Navbar email={session?.user.email}/>
         {children}
         </body>
